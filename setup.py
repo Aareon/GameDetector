0b85e02a91cd80d8746755124203bc4f9de37b58
Aareon Sullivan <askully13@gmail.com>
2023-06-10 13:59:00 -0500

    fix line length lint error

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name='GameDetector',
     packages=find_packages(include=['gamedetector']),
     version='0.1.0',
-    description='The GameDetector library allows you to detect a game within a folder, returning information like Steam AppId, game name, and version.',
+    description='The GameDetector library allows you to detect a game within a folder, returning information like Steam'
+            ' AppId, game name, and version.',
     long_description=(Path("__file__").parent / "README.md").read_text(),
     author='Aareon Sullivan',
     author_email="askully13@gmail.com",
