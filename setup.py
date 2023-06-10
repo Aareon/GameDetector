2ef78165744cc4bc59076dc44221afb69865760b
Aareon Sullivan <askully13@gmail.com>
2023-06-10 13:28:35 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+setup(
+    name='GameDetector',
+    packages=find_packages(include=['gamedetector']),
+    version='0.1.0',
+    description='The GameDetector library allows you to detect a game within a folder, returning information like Steam AppId, game name, and version.',
+    long_description=(Path("__file__").parent / "README.md").read_text(),
+    author='Aareon Sullivan',
+    author_email="askully13@gmail.com",
+    url="https://github.com/Aareon/GameDetector",
+    license='MIT',
+)
